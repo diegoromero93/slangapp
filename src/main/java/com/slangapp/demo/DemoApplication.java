14c2b61c77d649dfa3f7f37f8f66747963eb7a68
Diego Romero <diegor.romero@yahoo.com>
2020-07-16 12:30:17 -0600

    :blue_book: Adding Swagger for Documentation

--- src/main/java/com/slangapp/demo/DemoApplication.java
+++ src/main/java/com/slangapp/demo/DemoApplication.java
@@ -8,16 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.Executor;
 
